abc0dbb48649e95cdeb98ad00952080b0b30fa2a
jeffayad <80787079+jeffayad@users.noreply.github.com>
2024-08-31 00:12:43 -0400

    initial commit
    test

---
+++ json.js
@@ -0,0 +1,518 @@
+module.exports = {
+    // main dashboard page
+    Dashboard: {
+        video: [{
+            usEN: "Dashboard introduction",
+            zhCN: "界面介绍",
+            id: ""
+        }, {
+            usEN: "How to change UI",
+            zhCN: "如何改变界面功能",
+            id: ""
+        }],
+        setting: [{
+            usEN: "UI Setting",
+            zhCN: "界面设置",
+            name: "StationInterface"
+        }]
+    },
+    // for restaruant place order page
+    Menu: {
+        video: [{
+            usEN: "How to change order info",
+            zhCN: "如何更改订单信息"
+        }, {
+            usEN: "How to use timer",
+            zhCN: "如何使用定时功能"
+        }, {
+            usEN: "How to split ticket",
+            zhCN: "如何使用分单功能"
+        }, {
+            usEN: "How to view customer previous order",
+            zhCN: "如何查看客户的历史订单"
+        }, {
+            usEN: "How to place order faster",
+            zhCN: "如何更快的下单"
+        },],
+        setting: [{
+            usEN: "Menu Item Setup",
+            zhCN: "物品设置",
+            name: "MenuItemSetting"
+        }, {
+            usEN: "Menu Page Preference",
+            zhCN: "菜单偏好设置",
+            name: "MenuPreference"
+        }]
+    },
+    // item tutorial 
+    Item: {
+        video: [
+            // {
+            //     usEN: "",
+            //     zhCN: "",
+            //     item: "",
+            //     id: ""
+            // }
+        ],
+        setting: []
+    },
+    // for retail mode place order page
+    Retail: {
+        video: [{
+            usEN: "How to add barcode item",
+            zhCN: "如何添加物品条形码"
+        }, {
+            usEN: "How to search item",
+            zhCN: "如何搜索物品"
+        }],
+        setting: [{
+            usEN: "Cashdrawer Setting",
+            zhCN: "钱箱设置",
+            name: "CashdrawerConfig"
+        }, {
+            usEN: "Retail Config",
+            zhCN: "零售模式设置",
+            name: "RetailConfig"
+        }]
+    },
+    // customer information page
+    Information: {
+        video: [{
+            usEN: "How to autocomplete address",
+            zhCN: "如何自动填写地址信息"
+        }, {
+            usEN: "How to use tag function",
+            zhCN: "如何使用标签功能"
+        }, {
+            usEN: "How to switch customer address",
+            zhCN: "如何切换客户地址"
+        }],
+        setting: [{
+            usEN: "Setup Caller ID",
+            zhCN: "设置来电显示",
+            name: "CallerIdDevices"
+        }]
+    },
+    Table: {
+        video: [{
+            usEN: "About dine in page",
+            zhCN: "关于堂吃页面"
+        }, {
+            usEN: "How to place certain item first",
+            zhCN: "如何优先发送订单中的指定物品"
+        }, {
+            usEN: "How to swape item between tables",
+            zhCN: "如何在两个座位中交换物品"
+        }, {
+            usEN: "How to place order by seats",
+            zhCN: "如何按座位下单"
+        }, {
+            usEN: "How to place to go order",
+            zhCN: "如何下带走订单"
+        }, {
+            usEN: "How to combine tickets",
+            zhCN: "如何合并订单"
+        }],
+        setting: [{
+            usEN: "Table Setup",
+            zhCN: "座位设置",
+            name: "DineInTableSetup"
+        }, {
+            usEN: "Auto Gratuity Setup",
+            zhCN: "设置自动服务费",
+            name: "DineInPreference"
+        }]
+    },
+    History: {
+        video: [{
+            usEN: "How to edit ticket",
+            zhCN: "如何修改订单"
+        }, {
+            usEN: "How to split ticket",
+            zhCN: "如何分单"
+        }, {
+            usEN: "How to discount ticket",
+            zhCN: "如何为订单打折"
+        }, {
+            usEN: "How to use driver report",
+            zhCN: "如何使用司机报表"
+        }, {
+            usEN: "How to print sales report",
+            zhCN: "如何打印销售报表"
+        }]
+    },
+    BackOffice: {
+        video: [{
+            usEN: "About back office",
+            zhCN: "关于后台"
+        }, {
+            usEN: "About hardware warranty",
+            zhCN: "关于硬件保修"
+        }, {
+            usEN: "About software update",
+            zhCN: "关于软件更新"
+        }, {
+            usEN: "About teachnical support",
+            zhCN: "关于技术支持"
+        }],
+        setting: [{
+            usEN: "Purchase Extended Hardware Warranty",
+            zhCN: "购买延长硬件保修"
+        }, {
+            usEN: "Purchase Teachnical Support",
+            zhCN: "购买技术支持"
+        }]
+    },
+    StoreInfo: {
+        video: [{
+            usEN: "How to setup store infomation",
+            zhCN: "如何设置店面信息"
+        }],
+        setting: []
+    },
+    StoreOpenHour: {
+        video: [{
+            usEN: "How to setup business hour",
+            zhCN: "如何设置营业时间"
+        }, {
+            usEN: "About special business hour",
+            zhCN: "如何设置特殊日期营业时间"
+        }],
+        setting: []
+    },
+    TaxSetting: {
+        video: [{
+            usEN: "How to setup tax",
+            zhCN: "如何设置税率"
+        }],
+        setting: []
+    },
+    PaymentSetting: {
+        video: [{
+            usEN: "How to setup tax",
+            zhCN: "如何设置税率"
+        }],
+        setting: [{
+            usEN: "Receipt Setting",
+            zhCN: "订单收据设置",
+            name: "PrinterSetting"
+        }, {
+            usEN: "Credit Card Receipt Setting",
+            zhCN: "信用卡收据设置",
+            name: "TerminalDevice"
+        }, {
+            usEN: "Third Party Payment Setting",
+            zhCN: "第三方付款设置",
+            name: "ExternalPayment"
+        }]
+    },
+    TerminalDevice: {
+        video: [{
+            usEN: "How to setup a credit card terminal",
+            zhCN: "如何连接新刷卡机"
+        }, {
+            usEN: "How to adjust tip & batch",
+            zhCN: "如何添加小费和过账"
+        }, {
+            usEN: "Terminal common issue troubleshooting",
+            zhCN: "刷卡机常见问题指南"
+        }, {
+            usEN: "How to charge when no internet",
+            zhCN: "没有网络的情况下如何收取信用卡"
+        }, {
+            usEN: "Use customer feedback to improve service",
+            zhCN: "利用客户反馈来提高服务质量"
+        }],
+        setting: [{
+            usEN: "Set Station Credit Card Device",
+            zhCN: "设置工作站刷卡机",
+            name: "StationDevice"
+        }]
+    },
+    DeliverySetting: {
+        video: [{
+            usEN: "How to setup delivery fee",
+            zhCN: "如何设置送餐费"
+        }, {
+            usEN: "How to use dispatch function",
+            zhCN: "如何使用配送功能"
+        }, {
+            usEN: "How to obtain Google Map API",
+            zhCN: "如何获取Google 地图 API"
+        }],
+        setting: []
+    },
+    GiftcardSetting: {
+        video: [{
+            usEN: "How to setup giftcard",
+            zhCN: "如何设置礼物卡"
+        }],
+        setting: [{
+            icon: "fa-list",
+            usEN: "View Activated Giftcards",
+            zhCN: "查看已激活的礼物卡"
+        }]
+    },
+    DepartmentList: {
+        video: [{
+            usEN: "What is department",
+            zhCN: "什么是部门"
+        }],
+        setting: []
+    },
+    CouponList: {
+        video: [{
+            usEN: "How to setup coupon",
+            zhCN: "如何设置优惠券"
+        }],
+        setting: []
+    },
+    RewardProgram: {
+        video: [{
+            usEN: "How to setup reward program",
+            zhCN: "如何设置积分计划"
+        }],
+        setting: []
+    },
+    ReportSetting: {
+        video: [{
+            usEN: "What is auto E-mail report",
+            zhCN: "什么是自动邮件报表"
+        }],
+        setting: []
+    },
+    OtherSetting: {
+        video: [{
+            usEN: "What is device provision",
+            zhCN: "什么是设备认证"
+        }, {
+            usEN: "What is setting lock",
+            zhCN: "什么是设置锁定"
+        }],
+        setting: []
+    },
+    ApplicationPreference: {
+        video: [{
+            usEN: "What is Flag Keep Period",
+            zhCN: "什么是追溯期"
+        }],
+        setting: []
+    },
+    StationSetting: {
+        video: [{
+            usEN: "How to turn on all stations at once",
+            zhCN: "如何一次性启动所有工作站"
+        }, {
+            usEN: "How to auto lock station",
+            zhCN: "如何自动锁定工作站"
+        }],
+        setting: [{
+            usEN: "Dine In Setting",
+            zhCN: "堂吃设定",
+            name: ""
+        }]
+    },
+    StationDevice: {
+        video: [{
+            usEN: "How to connect external device",
+            zhCN: "如何连接外部设备"
+        }],
+        setting: [{
+            usEN: "Printer Setup",
+            zhCN: "设置打印机",
+            name: ""
+        }]
+    },
+    CashdrawerConfig: {
+        video: [{
+            usEN: "How to setup cashdrawer",
+            zhCN: "如何设置钱箱"
+        }]
+    },
+    CallerIdDevices: {
+        video: [{
+            usEN: "How to setup caller ID",
+            zhCN: "如何设置来电显示",
+        }],
+        setting: [{
+            usEN: "Buy Caller ID",
+            zhCN: "购买来电显示",
+            path: "/market/search/:callerid"
+        }]
+    },
+    MenuItemSetting: {
+        video: [{
+            usEN: "How to add a category",
+            zhCN: "如何添加一个物品类别"
+        }, {
+            usEN: "How to add an item",
+            zhCN: "如何添加一个物品"
+        }, {
+            usEN: "How to update group of items at once",
+            zhCN: "如何一次性更新整组的物品"
+        }, {
+            usEN: "What is item trend",
+            zhCN: "什么是物品热度"
+        }],
+        setting: [{
+            usEN: "Tax Category Setup",
+            zhCN: "税率种类设置",
+            name: "TaxSetting"
+        }, {
+            usEN: "Sorting Menu Item",
+            zhCN: "改变菜单物品排序",
+            name: "MenuPreference"
+        }]
+    },
+    RequestItemSetting: {
+        video: [{
+            usEN: "How to setup request item",
+            zhCN: "如何设置要求物品",
+            id: "X9nCFMMDOlo"
+        }],
+        setting: []
+    },
+    ThirdPartyProvider: {
+        video: [{
+            usEN: "How to setup platform surcharge fee",
+            zhCN: "如何设置平台费用"
+        }, {
+            usEN: "How to setup special price for third party",
+            zhCN: "如何设置第三方订单物品价格"
+        }],
+        setting: [{
+            usEN: "Set Third Party Item Price",
+            zhCN: "设置第三方价格",
+            name: "ItemPricesList"
+        }]
+    },
+    FeatureFlag: {
+        video: [{
+            usEN: "What is Enhance Profile",
+            zhCN: "什么是增强用户信息"
+        }, {
+            usEN: "How to use packer function",
+            zhCN: "如何使用打包功能"
+        }, {
+            usEN: "How to use kitchen display",
+            zhCN: "如何使用厨房显示功能"
+        }],
+        setting: []
+    },
+    TemplateSetting: {
+        video: [{
+            usEN: "What is item template",
+            zhCN: "什么是物品模板"
+        }, {
+            usEN: "How to setup item template",
+            zhCN: "如何设置物品模板"
+        }],
+        setting: []
+    },
+    ItemPricesList: {
+        video: [{
+            usEN: "How to setup special price for third party",
+            zhCN: "如何设置第三方订单物品价格"
+        }, {
+            usEN: "How to setup happy hour",
+            zhCN: "如何设置 happy hour"
+        }],
+        setting: []
+    },
+    ItemInventory: {
+        video: [{
+            usEN: "How to setup item inventory",
+            zhCN: "如何设置物品库存"
+        }],
+        setting: []
+    },
+    MenuConfig: {
+        video: []
+    },
+    MenuPreference: {
+        video: [{
+            usEN: "About menu page layout",
+            zhCN: "关于下单页面样式"
+        }],
+        setting: []
+    },
+    DineInTableSetup: {
+        video: [{
+            usEN: "How to setup table",
+            zhCN: "如何设置座位"
+        }],
+        setting: [{
+            usEN: "Switch seat capacity",
+            zhCN: "切换座位数量",
+            name: "DineInAdvance"
+        }]
+    },
+    BookingSetup: {
+        video: [{
+            usEN: "How to use booking function",
+            zhCN: "如何使用订位功能"
+        }],
+        setting: [{
+            usEN: "SMS Setup",
+            zhCN: "短信功能设置",
+            name: "TextMessageSetup"
+        }]
+    },
+    EmployeeList: {
+        video: [{
+            usEN: "How to add employee",
+            zhCN: "如何添加新员工"
+        }, {
+            usEN: "How to setup employee info",
+            zhCN: "如何设置员工信息"
+        }, {
+            usEN: "How to remove employee",
+            zhCN: "如何移除员工"
+        }],
+        setting: []
+    },
+    EmployeeSetting: {
+        video: [{
+            usEN: "How to setup employee permission",
+            zhCN: "如何设置员工权限"
+        }, {
+            usEN: "What is session report",
+            zhCN: "什么是时段报表"
+        }, {
+            usEN: "What is restricted account",
+            zhCN: "什么是限制账户"
+        }],
+        setting: [{
+            usEN: "Time Card Setting",
+            zhCN: "打卡设置",
+            name: "EmployeeTimecard"
+        }]
+    },
+    EmployeeSchedule: {
+        video: [{
+            usEN: "How to use employee schedule",
+            zhCN: "如何使用员工时间表"
+        }, {
+            usEN: "How to copy schedule",
+            zhCN: "如何复制员工时间表"
+        }],
+        setting: []
+    },
+    EmployeePayroll: {
+        video: [{
+            usEN: "How to use payroll function",
+            zhCN: "如何使用工资单功能"
+        }],
+        setting: []
+    },
+    EmployeeTimecard: {
+        video: [{
+            usEN: "How to setup timecard",
+            zhCN: "如何设置员工打卡"
+        }, {
+            usEN: "How to email timecard report",
+            zhCN: "如何发送员工打卡报表"
+        }],
+        setting: []
+    }
+}
